618ae7a8b1a0845189573f1aaf7bb16147ace6b1
svarvel <varvello@brave.com>
2021-11-03 12:27:29 -0400

    adding to DB

--- src/server/web-app.py
+++ src/server/web-app.py
@@ -19,8 +19,96 @@ import argparse
 import simplejson
 import subprocess
 import psycopg2
-import db_manager
-from db_manager import run_query, insert_data
+
+# connect to databse 
+def connect_to_database(): 
+	print("connecting to database")
+	conn = psycopg2.connect(database = 'mobile_testbed', user = 'nyu', password = 'pa0l1n0', 
+		host = '127.0.0.1', port = '5432', sslmode = 'require')
+	cur = conn.cursor()  
+	return True, conn, cur
+
+
+# run a generic query on the database
+def run_query(query):
+	info = None
+	msg = ''
+
+	# connecting to db 
+	connected = False 
+	conn = None
+	try:
+		connected, conn, cur = connect_to_database()
+
+	# manage exception 
+	except psycopg2.DatabaseError as e:
+		if conn:
+			conn.rollback()
+		msg = 'Issue connecting to database. Error %s' % e    
+
+	# add installed_addons to database 
+	if connected: 
+		try:
+			cur.execute(query)
+			info = cur.fetchall()
+			if len(info) > 0: 
+				msg = 'OK'
+			else: 
+				info = None
+				msg = 'WARNING -- no entry found'
+
+		# handle exception 
+		except Exception as e:
+			msg = 'Issue querying the database. Error %s' % e    
+
+		# always close connection
+		finally:
+			if conn:
+				conn.close()
+
+	# all good 
+	return info, msg 
+
+
+# insert status update from a device in the database
+def insert_data(tester_id, location, timestamp, data_json):
+	# local parameters 
+	msg = '' 
+
+	# connecting to db 
+	connected = False 
+	try:
+		connected, conn, cur = connect_to_database()
+
+	# manage exception 
+	except psycopg2.DatabaseError as  e:
+		if conn:
+			conn.rollback()
+		msg = 'Issue connecting to database. Error %s' % e    
+
+	# add installed_addons to database 
+	if connected: 
+		try:
+			insert_sql = "insert into status_update(tester_id, location, timestamp, data) values(%s, %s, %s, %s::jsonb);"
+			data = (tester_id, location, timestamp, json.dumps(data_json))
+			cur.execute(insert_sql, data)
+			msg = "status_update:all good" 	
+			
+			# make database changes persistent 
+			conn.commit()
+
+		# handle exception 
+		except Exception as e:
+			msg += 'Issue inserting into database. Error %s' % e    
+
+		# always close connection
+		finally:
+			if conn:
+				conn.close()
+
+	# all done 
+	return msg 
+
 
 # simple function to read json from a POST message 
 def read_json(req): 
