d8474fc25f5290fec0ac136ff5370e21e0682248
Matteo Varvello <varvello@gmail.com>
2021-11-11 21:51:16 +0400

    small fix

--- src/termux/speed-browse-test.sh
+++ src/termux/speed-browse-test.sh
@@ -152,10 +152,9 @@ run_speed_test(){
         then 
             echo "Image optimization to help OCR..."
             convert $screen_fast -type Grayscale "temp.${ext}"  
-			# TEMP disabling, crops too much 
-            #convert "temp.${ext}" -gravity South -chop 0x600 $screen_fast_processed
+            convert "temp.${ext}" -gravity South -chop 0x23 $screen_fast_processed
             #convert $screen_fast_processed -gravity North -chop 0x600 "temp.${ext}"  
-            mv "temp.${ext}" $screen_fast_processed
+            #mv "temp.${ext}" $screen_fast_processed
         fi 
         screen_fast_ocr="${res_folder}/ocr-fast-${curr_run_id}"
         tesseract -l eng $screen_fast_processed $screen_fast_ocr > /dev/null 2>&1
