1552534178ffed88a808e6e143bcf2f3f96433b0
Matteo Varvello <varvello@gmail.com>
2021-11-12 03:37:46 +0400

    testing wifi toggle

--- src/termux/test.sh
+++ src/termux/test.sh
@@ -3,16 +3,17 @@
 ## Date:   11/10/2021
 
 # import utilities files needed
-#script_dir=`pwd`
-#adb_file=$script_dir"/adb-utils.sh"
-#source $adb_file 
+script_dir=`pwd`
+adb_file=$script_dir"/adb-utils.sh"
+source $adb_file 
 
 # turn wifi on or off
 toggle_wifi(){
     opt=$1
-    echo "[toggle_wifi] Requested: $opt"
     sudo input keyevent KEYCODE_HOME
-    wifiStatus="on"
+    echo "[toggle_wifi] Requested: $opt"
+    
+	wifiStatus="on"
     #/usr/bin/ifconfig wlan0 | grep "inet" | grep "\." > /dev/null
     #if [ $? -eq 0 ]
     #then
@@ -52,9 +53,17 @@ toggle_wifi(){
     fi
 }
 
+sudo input keyevent KEYCODE_HOM
+echo "[toggle_wifi] swipe down"
+sudo input swipe 370 0 370 500
+sleep 5
+echo "[toggle_wifi] press"
+tap_screen 300 100 2
+echo "[toggle_wifi] swipe up"
+sudo input swipe 370 500 370 0
 
-toggle_wifi "off"
-timeout 5 /usr/bin/ifconfig wlan0 > wlan-info 2>&1
-echo $?
-sleep 5 
-toggle_wifi "on"
+#toggle_wifi "off"
+#timeout 5 /usr/bin/ifconfig wlan0 > wlan-info 2>&1
+#echo $?
+#sleep 5 
+#toggle_wifi "on"
