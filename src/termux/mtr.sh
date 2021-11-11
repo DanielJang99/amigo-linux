551b112eba7fe6bbb3833b7cd5de09e56778ed80
Matteo Varvello <varvello@gmail.com>
2021-11-11 23:40:18 +0400

    improving mtr with ipv4 and ipv6

--- src/termux/mtr.sh
+++ src/termux/mtr.sh
@@ -1,21 +1,31 @@
 #!/data/data/com.termux/files/usr/bin/bash
+
+test(){
+	num=10
+	prefix=$2
+	sudo mtr -r4wc $num $1  >  $res_dir/$prefix-ipv4-$ts.txt 2>&1
+	ping6 -c 3 $1 > /dev/null 2>&1
+	if [ $? -eq 0 ] 
+	then 
+		sudo mtr -r6wc $num $1   >  $res_dir/$prefix-ipv6-$ts.txt 2>&1
+	fi 
+}
+
 suffix=`date +%d-%m-%Y`
 res_dir="pinglogs/$suffix"
 ts=`date +%s`
-num=10
 mkdir -p $res_dir
-
 t_s=`date +%s`
 echo "Starting MTR reporting..."
 
 # popular providers
-sudo mtr -rwc $num google.com   >  $res_dir/google-$ts.txt 2>&1
-sudo mtr -rwc $num facebook.com >  $res_dir/facebook-$ts.txt  2>&1
-sudo mtr -rwc $num amazon.com   >  $res_dir/amazon-$ts.txt 2>&1
+test google.com google 
+test facebook.com facebook
+test amazon.com amazon
 
-#DNS 
-sudo mtr -rwc $num 8.8.8.8 > $res_dir/google-dns-$ts.txt 2>&1
-sudo mtr -rwc $num 1.1.1.1 > $res_dir/cloudflare-dns-$ts.txt 2>&1
+# popular DNS
+test 8.8.8.8 google-dns
+test 1.1.1.1 cloudflare-dns
 #sudo mtr -rwc $num 8.8.8.8 > $res_dir/ 2>&1
 
 # logging 
