0ea05325dc3ec41c1b9e6d553b9b1b50f4a485bb
Matteo Varvello <varvello@gmail.com>
2021-11-12 03:32:35 +0400

    testing wifi toggle

--- src/termux/test.sh
+++ src/termux/test.sh
@@ -3,9 +3,55 @@
 ## Date:   11/10/2021
 
 # import utilities files needed
-script_dir=`pwd`
-adb_file=$script_dir"/adb-utils.sh"
-source $adb_file 
+#script_dir=`pwd`
+#adb_file=$script_dir"/adb-utils.sh"
+#source $adb_file 
+
+# turn wifi on or off
+toggle_wifi(){
+    opt=$1
+    myprint "[toggle_wifi] Requested: $opt"
+    sudo input keyevent KEYCODE_HOME
+    wifiStatus="off"
+    /usr/bin/ifconfig wlan0 | grep "inet" | grep "\." > /dev/null
+    if [ $? -eq 0 ]
+    then
+        wifiStatus="on"
+    fi
+    myprint "[toggle_wifi] Requested: $opt Status: $wifiStatus"
+    if [ $opt == "on" ]
+    then
+        if [ $wifiStatus == "off" ]
+        then
+            myprint "[toggle_wifi] swipe down"
+            sudo input swipe 370 0 370 500
+            sleep 5
+            myprint "[toggle_wifi] press"
+            tap_screen 300 100 2
+            myprint "[toggle_wifi] swipe up"
+            sudo input swipe 370 500 370 0
+        else
+            myprint "Requested wifi ON and it is already ON"
+        fi
+    elif [ $opt == "off" ]
+    then
+        if [ $wifiStatus == "on" ]
+        then
+            myprint "[toggle_wifi] swipe down"
+            sudo input swipe 370 0 370 500
+            sleep 5
+            myprint "[toggle_wifi] press"
+            tap_screen 300 100 2
+            myprint "[toggle_wifi] swipe up"
+            sudo input swipe 370 500 370 0
+        else
+            myprint "Requested wifi OFF and it is already OFF"
+        fi
+    else
+        myprint "Option $opt not supported (on/off)"
+    fi
+}
+
 
 echo "toggle_wifi off"
 toggle_wifi "off"
