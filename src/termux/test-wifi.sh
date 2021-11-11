d9c6f7c04f15bf89cf5f3337e73dfb2a25d98a35
Matteo Varvello <varvello@gmail.com>
2021-11-12 03:44:52 +0400

    damns

--- src/termux/test-wifi.sh
+++ src/termux/test-wifi.sh
@@ -12,6 +12,17 @@ sleep 2
 echo "[toggle_wifi] swipe up"
 sudo input swipe 370 500 370 0
 
+echo "[toggle_wifi] swipe down"
+sudo input swipe 370 0 370 500
+sleep 5
+echo "[toggle_wifi] press"
+sudo input tap 300 100
+sleep 2 
+echo "[toggle_wifi] swipe up"
+sudo input swipe 370 500 370 0
+
+
+
 #toggle_wifi "off"
 #timeout 5 /usr/bin/ifconfig wlan0 > wlan-info 2>&1
 #echo $?
