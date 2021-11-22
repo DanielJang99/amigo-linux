329eb122ea0af5f38ab0acc32ea34075d78424b0
svarvel <varvello@brave.com>
2021-11-22 11:32:05 -0500

    improving

--- src/setup/setup-phone-adb.sh
+++ src/setup/setup-phone-adb.sh
@@ -13,48 +13,117 @@ then
 fi 
 
 # parameters 
-device_id=$1                     # device to be prepped 
-ssh_key="id_rsa_mobile"          # unique key used for both SSH and GITHUB 
-password="termux"                # default password
+device_id=$1                      # device to be prepped 
+ssh_key="id_rsa_mobile"           # unique key used for both SSH and GITHUB 
+password="termux"                 # default password
+apk="F-Droid.apk"                 # FDroid APK to be installed
+fdroid_pack="org.fdroid.fdroid"   # fdroid package 
+termux_pack="com.termux"          # termux package 
+termux_boot="com.termux.boot"     # termux boot package 
+termux_api="com.termux.api"       # termux API package 
 
 # install Fdroid
-adb -s $device_id push F-Droid.apk /data/local/tmp/
-adb -s $device_id shell pm install -t /data/local/tmp/F-Droid.apk
+adb -s $device_id shell 'pm list packages -f' | grep $fdroid_pack > /dev/null
+to_install=$?
+if [ $to_install -eq 1 ]
+then 
+    if [ ! -f $apk ]
+    then 
+        echo "ERROR missing $apk"
+        exit -1 
+    fi 
+    adb -s $device_id push $apk /data/local/tmp/
+    adb -s $device_id shell pm install -t /data/local/tmp/$apk
+    adb -s $device_id shell 'pm list packages -f' | grep $fdroid_pack > /dev/null
+    if [ $to_install -eq 1 ]
+    then 
+        echo "ERROR installing $apk"
+        exit -1
+    else 
+        echo "$apk ($fdroid_pack) was installed correctly"
+    fi 
+fi 
+exit 0 
 
 # install termux from FDroid
 # need to activate install unknown apps 
-adb -s $device_id shell monkey -p org.fdroid.fdroid 1
-adb -s $device_id shell "input tap 630 1080"
-adb -s $device_id shell input text "termux\ terminal\ emulator"
-adb -s $device_id shell "input tap 626 256"
-adb -s $device_id shell "input tap 560 780"
-adb -s $device_id shell "input tap 640 400"
-adb -s $device_id shell "input keyevent KEYCODE_BACK"
-adb -s $device_id shell "input tap 620 1200"
-adb -s $device_id shell "input tap 590 130"
+adb -s $device_id shell 'pm list packages -f' | grep $termux_pack> /dev/null
+to_install=$?
+if [ $to_install -eq 1 ]
+then 
+    adb -s $device_id shell monkey -p $fdroid_pack 1
+    adb -s $device_id shell "input tap 630 1080"
+    adb -s $device_id shell input text "termux\ terminal\ emulator"
+    adb -s $device_id shell "input tap 626 256"
+    adb -s $device_id shell "input tap 560 780"
+    adb -s $device_id shell "input tap 640 400"
+    adb -s $device_id shell "input keyevent KEYCODE_BACK"
+    adb -s $device_id shell "input tap 620 1200"
+    adb -s $device_id shell "input tap 590 130"
+    adb -s $device_id shell 'pm list packages -f' | grep $termux_pack> /dev/null
+    if [ $to_install -eq 1 ]
+    then 
+        echo "ERROR installing termux!"
+        exit -1 
+    else 
+        echo "termux ($termux_pack) was installed correctly"
+    fi
+fi 
+exit 0 
 
-# install boot and termux api
-adb -s $device_id  shell input text "termux\ api"
-sleep 10 
-adb -s $device_id shell "input keyevent KEYCODE_ENTER"
-adb -s $device_id shell "input tap 626 256"
-sleep 10 
-adb -s $device_id shell "input tap 626 256"
-sleep 2 
-adb -s $device_id shell "input tap 656 745"
-sleep 10 
-adb -s $device_id shell "input tap 590 130"
-adb -s $device_id  shell input text "termux\ boot"
-sleep 10 
-adb -s $device_id shell "input keyevent KEYCODE_ENTER"
-adb -s $device_id shell "input tap 626 256"
-sleep 10 
-adb -s $device_id shell "input tap 626 256"
-sleep 2 
-adb -s $device_id shell "input tap 656 745"
-sleep 10 
-adb -s $device_id shell "input tap 590 130"
-adb -s $device_id shell "input keyevent KEYCODE_HOME"
+# install termux api 
+adb -s $device_id shell 'pm list packages -f' | grep $termux_api> /dev/null
+to_install=$?
+if [ $to_install -eq 1 ]
+then 
+    adb -s $device_id shell monkey -p $fdroid_pack 1
+    adb -s $device_id  shell input text "termux\ api"
+    sleep 10 
+    adb -s $device_id shell "input keyevent KEYCODE_ENTER"
+    adb -s $device_id shell "input tap 626 256"
+    sleep 10 
+    adb -s $device_id shell "input tap 626 256"
+    sleep 2 
+    adb -s $device_id shell "input tap 656 745"
+    sleep 10 
+    adb -s $device_id shell "input tap 590 130"
+    adb -s $device_id shell 'pm list packages -f' | grep $termux_api > /dev/null
+    if [ $to_install -eq 1 ]
+    then 
+        echo "ERROR installing termux-api!"
+        exit -1 
+    else 
+        echo "termux-api ($termux_api) was installed correctly"
+    fi
+fi 
+exit 0 
+
+# install termux boot 
+adb -s $device_id shell 'pm list packages -f' | grep $termux_boot> /dev/null
+to_install=$?
+if [ $to_install -eq 1 ]
+then 
+    adb -s $device_id  shell input text "termux\ boot"
+    sleep 10 
+    adb -s $device_id shell "input keyevent KEYCODE_ENTER"
+    adb -s $device_id shell "input tap 626 256"
+    sleep 10 
+    adb -s $device_id shell "input tap 626 256"
+    sleep 2 
+    adb -s $device_id shell "input tap 656 745"
+    sleep 10 
+    adb -s $device_id shell "input tap 590 130"
+    adb -s $device_id shell "input keyevent KEYCODE_HOME"
+    adb -s $device_id shell 'pm list packages -f' | grep $termux_boot > /dev/null
+    if [ $to_install -eq 1 ]
+    then 
+        echo "ERROR installing termux_boot!"
+        exit -1 
+    else 
+        echo "termux-api ($termux_boot) was installed correctly"
+    fi
+fi 
+exit 0 
 
 # install SSH on termux (might indeed need human help)
 adb -s $device_id shell monkey -p com.termux 1
