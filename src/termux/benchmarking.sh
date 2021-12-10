4ad52de68df0485f419a655d16828d3becd89726
Matteo Varvello <varvello@gmail.com>
2021-12-10 20:23:50 +0400

    fix

--- src/termux/benchmarking.sh
+++ src/termux/benchmarking.sh
@@ -1,4 +1,4 @@
-#!/data/data/com.termux/files/usr/bin/env bash
+#!/bin/bash
 ## NOTE: benchmarking the server 
 ## Author: Matteo Varvello (matteo.varvello@nokia.com)
 ## Date: 12/10/2021
@@ -24,16 +24,13 @@ EOF
 }
 
 # main code 
-for((i=0; i<100; i++))
-do 
-    suffix=`date +%d-%m-%Y`
-    current_time=`date +%s`
-    let "uid++"
-    uptime_info=`uptime`
-    msg="benchmarking"
-    t_s=`date +%s`
-    timeout 10 curl -s -H "Content-Type:application/json" -X POST -d "$(generate_post_data)" https://mobile.batterylab.dev:8082/status
-    t_e=`date +%s`
-    let "t_p = t_e - t_s"
-    echo "[$i] Duration: $t_p"
-done
+uid=$1
+suffix=`date +%d-%m-%Y`
+current_time=`date +%s`
+uptime_info=`uptime`
+msg="benchmarking"
+t_s=`date +%s`
+timeout 30 curl -s -H "Content-Type:application/json" -X POST -d "$(generate_post_data)" https://mobile.batterylab.dev:8082/benchmarking
+t_e=`date +%s`
+let "t_p = t_e - t_s"
+echo "[$uid] Duration: $t_p"
