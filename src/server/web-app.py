d1dba4ce4280b469a322995e1aa1ab8a9388a968
svarvel <varvello@brave.com>
2021-12-10 11:29:06 -0500

    introducing connection pool to db

--- src/server/web-app.py
+++ src/server/web-app.py
@@ -89,7 +89,7 @@ def web_app():
         'server.socket_port': port, 
         'server.ssl_module':'builtin',
         'server.ssl_certificate':'certificate.pem',
-        'server.thread_pool': 50,
+        'server.thread_pool': 100,
     }
     cherrypy.config.update(server_config)
 
