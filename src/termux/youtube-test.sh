9a78915b32abc3d6c2e707eb1fa10d399384d77a
svarvel <varvello@brave.com>
2021-12-11 13:33:51 -0500

    bug fix

--- src/termux/youtube-test.sh
+++ src/termux/youtube-test.sh
@@ -33,10 +33,12 @@ send_report(){
 # activate stats for nerds  
 activate_stats_nerds(){
 	myprint "Activating stats for nerds!! -- FIXME"
-	tap_screen 680 105  3
-	#sudo input tap 680 105 && sleep 0.1 && sudo input tap 680 105
-	tap_screen 680 105  3
-	tap_screen 370 1125 3
+	#tap_screen 680 105  3
+	#tap_screen 680 105  3	
+	sudo input tap 680 105 && sleep 0.1 && sudo input tap 680 105
+	sleep 3
+	#tap_screen 370 1125 3
+	tap_screen 370 1024 1  # what happened????
 }
 
 # script usage
