378463ccaa38dfb82d581da8c3898b5e17723748
svarvel <varvello@brave.com>
2021-12-14 16:44:20 -0500

    modified package check to report to server

--- src/setup/package-check.sh
+++ src/setup/package-check.sh
@@ -1,4 +1,19 @@
-#!/bin/bash
+#!/data/data/com.termux/files/usr/bin/env bash
+
+# generate data to be POSTed to my server
+generate_post_data(){
+  cat <<EOF
+    {
+    "today":"${suffix}",
+    "timestamp":"${current_time}",
+    "uid":"${uid}",
+    "debug":"${debug}",
+    "msg":"${msg}"
+    }
+EOF
+}
+
+# check packages 
 pkg upgrade -y
 pkg install -y termux-api tsu root-repo mtr python tcpdump tshark ffmpeg imagemagick
 python -m pip install --upgrade pip
@@ -7,3 +22,10 @@ pip install wheel
 pip install pillow
 cd ../termux 
 ./check-visual.sh
+
+# main code 
+uid=`termux-telephony-deviceinfo | grep device_id | cut -f 2 -d ":" | sed s/"\""//g | sed s/","//g | sed 's/^ *//g'`
+suffix=`date +%d-%m-%Y`
+current_time=`date +%s`
+msg="DONE"
+timeout 30 curl -s -H "Content-Type:application/json" -X POST -d "$(generate_post_data)" https://mobile.batterylab.dev:8082/benchmarking
