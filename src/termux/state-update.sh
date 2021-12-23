b451cec65f9cb8a561128605d10ccde931c58bf0
svarvel <varvello@brave.com>
2021-12-23 14:26:45 -0500

    bug fix

--- src/termux/state-update.sh
+++ src/termux/state-update.sh
@@ -806,9 +806,12 @@ do
 	locked="false"
 	if [  -f ".locked" ]
 	then 
-		locked="true"	fi 
+		locked="true"
+	fi 
 
+	# logging 
 	myprint "TimeFromLastNetLong:$time_from_last_net sec TimeFromLastNetShort:$time_from_last_net_short sec TimeFromLastNetForced:$time_from_last_net_forced sec ShouldRunIfTime:$net_status RunningNetProc:$num LockedStatus:$locked"
+	
 	# 1) flag set, 2) no previous running, 3) connected (basic checks to see if we should run)
 	if [ $net_status == "true" -a $num -eq 0 -a $def_iface != "none" -a $locked == "false" ]  
 	then
