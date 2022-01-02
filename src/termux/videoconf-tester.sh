4bcb5d0e1940551a47dd5387c245997cd003e583
svarvel <varvello@brave.com>
2022-01-02 16:14:48 -0500

    more time before full screen

--- src/termux/videoconf-tester.sh
+++ src/termux/videoconf-tester.sh
@@ -863,10 +863,8 @@ then
 		#let "disc_time += 10"    	
 		myprint "Tapping screen for attempt at full screen!" ## single user 
 		# more than one user (assuming video is in top left corner)		
-		sudo input tap 200 400 &
-		sleep 0.2 
-		sudo input tap 200 400 #435  
-		
+		sudo input tap 200 400 		
+		sudo input tap 200 400 & sleep 0.1 sudo input tap 200 400 
 		#sudo input tap $x_center $y_center & sleep 0.1; sudo input tap $x_center $y_center # single user 
 		#sleep 5 
 		#let "disc_time += 5"
