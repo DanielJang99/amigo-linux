f5aac3e59d3f213e43ae407de680e6d22a822067
svarvel <varvello@brave.com>
2022-01-03 10:06:46 -0500

    fixing volume bug

--- src/termux/videoconf-tester.sh
+++ src/termux/videoconf-tester.sh
@@ -13,6 +13,7 @@ function ctrl_c() {
 # lower all the volume 
 lower_volume(){
 	myprint "Making sure volume is off"
+	sudo media volume --stream 0 --set 0    # call volume
 	sudo media volume --stream 3 --set 0    # media volume
 	sudo media volume --stream 1 --set 0	# ring volume
 	sudo media volume --stream 4 --set 0	# alarm volume
