c5e8e9057cb6c3748abccbef6ad4a6d26f0844c7
DanielJang99 <daniel.jang.0621@gmail.com>
2023-11-29 17:50:22 +0400

    check data usage for speedtest on mobile data

---
+++ src/termux/v2/run_speedtest.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+import subprocess
+import sys
+import json
+from datetime import date 
+
+DAILY_MOBILE_DATA_LIMIT = 300000000 # limit = 300mb 
+
+class SpeedtestOptions:
+    BOTH = "speedtest-cli --json"
+    DOWNLOAD = "speedtest-cli --json --no-upload"
+    UPLOAD = "speedtest-cli --json --no-download"
+
+def run_test(cmd, outputFile):
+    run = subprocess.run(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    if run.returncode != 0:
+        exit(1) 
+    out = run.stdout.decode('utf-8')
+    with open(outputFile, "w") as f:
+        f.write(out)
+    out_json = json.loads(out)
+    bytesSent=out_json["bytes_sent"]
+    bytesReceived=out_json["bytes_received"]
+    return bytesSent+bytesReceived
+
+def write_logs(logfile, msg):
+    with open(logfile, "w") as f:
+        f.write(msg)
+
+def main(network_type, outputfile, logfile=None, options=None):
+    if network_type == "WIFI":
+        run_test(SpeedtestOptions.BOTH, outputfile)
+    else:
+        today = date.today().strftime("%d-%m-%Y")
+        if not options:
+            test_bytes = run_test(SpeedtestOptions.BOTH, outputfile)
+            write_logs(logfile, f"{today} {test_bytes}")
+            return 
+
+        options_split = options.split(" ")
+        last_test_day, bytes_used = options_split[0], options_split[1]
+
+        # run both download & upload test if this is the first test of the day
+        if (last_test_day != today):
+            test_bytes = run_test(SpeedtestOptions.BOTH, outputfile)
+            write_logs(logfile, f"{today} {test_bytes}")
+            return 
+        
+        # skip if we have already used more than 300mb for speedtest today
+        if bytes_used > DAILY_MOBILE_DATA_LIMIT:
+            print("DATA_EXCEEDED")
+            return
+        
+        test_bytes = run_test(SpeedtestOptions.BOTH, outputfile)
+        write_logs(logfile, f"{today} {test_bytes+bytes_used}")
+        
+        
+        # # run download if our previous test today was upload, and vice versa. Exit if it exceeds daily data limit 
+        # if last_test_type == "up":
+        #     test_bytes = run_test(SpeedtestOptions.DOWNLOAD, outputfile)
+        #     if test_bytes + bytes_used < DAILY_MOBILE_DATA_LIMIT:
+        #         test_bytes += run_test(SpeedtestOptions.UPLOAD, outputfile)
+        #         write_logs(f"{today} {test_bytes+bytes_used} up {network_type}")
+        #     else:
+        #         write_logs(f"{today} {test_bytes+bytes_used} down {network_type}")
+
+
+
+
+if '__main__' == __name__:    
+    if len(sys.argv) == 3:
+        main(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) == 4:
+        main(sys.argv[1], sys.argv[2], sys.argv[3])
+    elif len(sys.argv) == 5:
+        main(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+    else:
+        print("Invalid Number of Arguments")
+        exit(1)
--- src/termux/v2/speed-test.sh
+++ src/termux/v2/speed-test.sh
@@ -5,6 +5,9 @@
 adb_file=`pwd`"/adb-utils.sh"
 source $adb_file
 
+suffix=`date +%d-%m-%Y`
+id=`date +%s`
+
 while [ "$#" -gt 0 ]
 do
     case "$1" in
@@ -19,11 +22,67 @@ do
     esac
 done
 
+run_speedtest(){
+    cmd="python ./v2/run_speedtest.py $1"
+    run=`( $cmd )`
+    if [ $? -ne 0 ]
+    then
+        myprint "Speedtest Failed"
+    else
+        if [[ "$run" == "DATA_EXCEEDED" ]];then
+            myprint "Speedtest Skipped"
+        else 
+            myprint "Speedtest Success"
+        fi
+    fi
+}
+
 network_type=`get_network_type`
 network_ind=`echo $network_type | cut -f 1 -d "_"`
-id="${id}_${network_ind}"
+network_ind=`echo "$network_ind// /-"`
 res_folder="speedtest-cli-logs/${suffix}"
 mkdir -p $res_folder
-speedtest-cli --json > "${res_folder}/speedtest-$id.json"
-gzip "${res_folder}/speedtest-$id.json"
+testId="${id}_${network_ind}"
+output_path="${res_folder}/speedtest-$testId.json"
+
+if [[ "$network_ind" == "WIFI" ]];
+then
+    args="$network_ind $output_path"
+    run_speedtest "$args"
+else
+    subscriptions_file="/storage/emulated/0/Android/data/com.example.sensorexample/files/subscriptions.txt"
+    simNum=`su -c cat $subscriptions_file | grep -n $network_ind | cut -f1 -d:`
+    logFile=".last_mobile_speedtest_$simNum"
+    if [ -f $logFile ]
+    then
+        log=`cat $logFile`
+        args="$network_ind $output_path $logFile $log"
+        run_speedtest "$args"
+    else
+        args="$network_ind $output_path $logFile"
+        run_speedtest "$args"
+    fi
+fi
+
+# if [ -f $logFile ]
+#     lastMobSTest=`cat $logFile`
+#     var=( $(echo ${d} | awk '{print $1,$2,$3}') )
+#     last_test_tp="${var[0]}"
+#     current_tp=`date +%s`
+#     let "time_elapsed_since_last = current_tp-last_test_tp"
+#     if [ $time_elapsed_since_last -lt 86400 ];
+#     then
+#         bytesUsedToday="${var[1]}"
+#         # check threshold 
+#     else
+#         run_speedtest
+#         # overwrite .last_mobile_speedtest 
+#     fi
+# fi
+
+# testId="${id}_${network_ind}"
+# res_folder="speedtest-cli-logs/${suffix}"
+# mkdir -p $res_folder
+# speedtest-cli --json > "${res_folder}/speedtest-$testId.json"
+# gzip "${res_folder}/speedtest-$testId.json"
 
