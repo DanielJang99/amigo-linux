2d3f66cabafd01caac524c4199de2db8474d2f9b
Yasir Zaki <daniel.jang.0621@gmail.com>
2025-06-05 17:39:34 +0400

    edit config file and add a script to start container

---
+++ container/start_container.sh
@@ -0,0 +1,16 @@
+if [ $# -ne 1 ]; then
+    echo "Error: Please provide a unique MACHINE_ID as argument"
+    echo "Usage: $0 <MACHINE_ID>"
+    exit 1
+fi
+
+MACHINE_ID=$1
+
+echo "Starting container..."
+
+if [ -z "$MACHINE_ID" ]; then
+    echo "Error: MACHINE_ID is not set"
+    exit 1
+fi
+
+docker run -d -e HOST_MACHINE_ID=$MACHINE_ID --platform linux/amd64 --network host --cap-add CAP_NET_ADMIN hsj276/amigo-linux-image:latest
